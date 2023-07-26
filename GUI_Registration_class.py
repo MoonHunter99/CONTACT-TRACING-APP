cd3e058bd06ece40586b9c570067f277355be543
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-07-26 09:31:22 +0800

    created pseudocode except for main program

---
+++ GUI_Registration_class.py
@@ -0,0 +1,6 @@
+# import customtkinter
+# import csv
+# create a class for the registration segment
+# create a constructor of a gui to use for getting the information of registration
+# create a method for starting the mainloop of the window
+# create a method of getting the information and write it in a excel file
